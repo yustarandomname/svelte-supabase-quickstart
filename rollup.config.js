156a4f269e62f763bad50e69653fe210c760209c
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-06-05 19:50:47 +0200

    Setup app

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,10 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
+import { config } from 'dotenv';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json'
+
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -37,6 +41,16 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+			__api: JSON.stringify({
+				env: {
+					isProd: production,
+					...config().parsed // attached the .env config
+				}
+			}),
+		}),
+		json(),
+
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+	import {user} from "./sessionStore"
+	import {supabase} from "./supabaseClient"
+	import Auth from "./Auth.svelte"
+	import Profile from "./Profile.svelte"
+
+	user.set(supabase.auth.user())
+
+	supabase.auth.onAuthStateChange((_, session) => {
+		user.set(session.user)
+	})
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="container" style="padding: 50px 0 100px 0;">
+	{#if $user}
+			<Profile />
+	{:else}
+			<Auth />
+	{/if}
+</div>
