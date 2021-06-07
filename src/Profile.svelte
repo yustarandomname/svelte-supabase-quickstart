b3ebdfeaf65570c904bb57eedf0b25abdda814a7
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-06-07 11:36:24 +0200

    added bonus

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -1,6 +1,7 @@
 <script>
   import { supabase } from './supabaseClient'
   import { user } from './sessionStore'
+  import Avatar from './Avatar.svelte'
 
   let loading = true
   let username = null
@@ -72,6 +73,8 @@
 </script>
 
 <form use:getProfile class="form-widget" on:submit|preventDefault={updateProfile}>
+  <Avatar bind:path={avatar_url} on:upload={updateProfile} />
+
   <div>
     <label for="email">Email</label>
     <input id="email" type="text" value={$user.email} disabled />
